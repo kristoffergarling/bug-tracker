f9164801660d22eb6258d020b8207d8402501781
Kristoffer Garling <kristoffer.g.bengtsson@gmail.com>
2022-09-13 08:46:26 +0200

    test

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,20 @@
-import app from "./app";
-import { connectToDataBase } from "./db";
+const express = require('express');
+const mongoose = require("mongoose");
+const app = express();
 
-connectToDataBase();
+const uri = "mongodb+srv://garlingdev:igelkott55@cluster0.jfjeg.mongodb.net/?retryWrites=true&w=majority";
 
-app.listen(process.env.PORT, () => {
-    console.log("Server is running on PORT: " + process.env.PORT );
+async function connect() {
+    try {
+        await mongoose.connect(uri);
+        console.log("connected to mongodb");
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// connect();
+
+app.listen(8000, () => {
+    console.log("Server is running on port 8000");
 })
